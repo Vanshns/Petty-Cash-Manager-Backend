17827467665bdf602e3f5d7e8152d473788688c4
safwan <safwan.mirza@bookeventz.com>
2026-05-22 12:09:49 +0530

    added yaml and log in server.js

--- src/server.js
+++ src/server.js
@@ -3,12 +3,20 @@ require("dotenv").config();
 const app = require("./app");
 const logger = require("./config/logger");
 
+const prisma = require("./config/db");
+
 const PORT = process.env.PORT || 5000;
 
 // app.listen(PORT, () => {
 //   logger.info(`Server running on port ${PORT}`);
 // });
 
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, "0.0.0.0", async () => {
   logger.info(`Server running on port ${PORT}`);
+  try {
+    await prisma.$connect();
+    logger.info("Database connected successfully!");
+  } catch (error) {
+    logger.error("Database connection failed:", error);
+  }
 });
